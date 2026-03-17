6eba2ca26597793f1c39b3142b6c791f4222caf0
zzz-ochiai <zzz.ochiai@gmail.com>
2026-03-18 01:40:55 +0900

    Add admin user management feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get "tickets/index"
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  devise_for :users
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
@@ -15,6 +13,9 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   root "tickets#index"
-
-  resources :tickets, only: [:index, :update]
+  
+  namespace :admin do
+    resources :users, only: [:index, :show]
+  end
+  
 end
--- db/migrate/20260317150453_add_admin_to_users.rb
+++ db/migrate/20260317150453_add_admin_to_users.rb
@@ -1,5 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration[8.1]
   def change
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false, null: false
+    # defaultは新しいユーザーがadminでないことを保証し、null: falseはadminカラムが必ず値を持つことを保証します。
   end
 end
