e4c1206bd80a75391276eb99675817bbfcf7fe48
zzz-ochiai <zzz.ochiai@gmail.com>
2026-04-27 01:30:58 +0900

    オフライン時のチケット使用処理を改善

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,19 +7,19 @@ class TicketsController < ApplicationController
   def update
     @ticket = current_user.tickets.find(params[:id])
 
-    if @ticket.used?
-      redirect_to tickets_path, alert: "このチケットはすでに使用済みです"
-      return
-    end
+    client_time = Time.parse(params[:updated_at])
 
-    respond_to do |format|
-      if @ticket.update(used: true)
-        format.html { redirect_to tickets_path, notice: "チケットを使用済みにしました" }
-        format.json { render json: { status: "ok", ticket: @ticket } }
-      else
-        format.html { render :index, status: :unprocessable_entity }
-        format.json { render json: { errors: @ticket.errors.full_messages }, status: :unprocessable_entity }
-      end
+    if client_time > @ticket.updated_at
+      @ticket.update(ticket_params)
+      render json: @ticket
+    else
+      render json: { error: "古い更新です" }, status: :conflict
     end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:used)
   end
 end
