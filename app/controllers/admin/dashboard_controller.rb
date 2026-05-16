1cf57fddb00c6d0e4aa86bf630c5c01fb451db92
zzz-ochiai <zzz.ochiai@gmail.com>
2026-05-16 13:15:35 +0900

    Restrict admin dashboard to approved admins

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,16 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin!
+  before_action :check_admin_status
 
   def index
+  end
+
+  private
+
+  def check_admin_status
+    return if current_admin.status == "approved"
+
+    sign_out current_admin
+    redirect_to new_admin_session_path, alert: "承認待ちです"
   end
 end
