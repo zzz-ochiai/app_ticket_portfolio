c86dd5f039d89c9eacd0df9cf51e941b1fc8c329
zzz-ochiai <zzz.ochiai@gmail.com>
2026-05-21 21:34:41 +0900

    Improve admin UI and add bidirectional ticket status control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      super
+    end
+  end
 end
